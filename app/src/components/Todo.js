74c37a3ed3fd0c721c351f49376fccfeed2d24df
Shaohua <huang@sg.ibm.com>
2018-04-08 22:45:55 +0800

    add styled-components

--- app/src/components/Todo.js
+++ app/src/components/Todo.js
@@ -1,14 +1,21 @@
 import React from "react"
+import styled from "styled-components"
+import muiThemeable from "material-ui/styles/muiThemeable"
 
-const Todo = ({ onClick, completed, text }) => (
-    <li
+const Todo = ({ onClick, completed, text, muiTheme }) => (
+    <StyledLi
         onClick={onClick}
-        style={{
-            textDecoration: completed ? "line-through" : "none"
-        }}
+        data-completed={completed}
+        data-theme={muiTheme}
     >
         {text}
-    </li>
+    </StyledLi>
 )
 
-export default Todo
+const StyledLi = styled.li`
+    text-decoration: ${props =>
+        props["data-completed"] ? "line-through" : "none"};
+    background: ${props => props["data-theme"].background};
+`
+
+export default muiThemeable()(Todo)
