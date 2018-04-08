a17c7c957ad1c6c92373d8b23ea8e43ca59f2891
Shaohua <huang@sg.ibm.com>
2018-04-08 22:35:27 +0800

    add material UI

--- app/src/index.js
+++ app/src/index.js
@@ -3,19 +3,28 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import getMuiTheme from "material-ui/styles/getMuiTheme"
+import injectTapEventPlugin from "react-tap-event-plugin"
+import Theme from "./theme"
+
 import App from "./App"
 import configureStore from "./configureStore"
+
+injectTapEventPlugin()
 
 const store = configureStore()
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/:filter" component={App} />
-            </Switch>
-        </Router>
-    </Provider>,
+    <MuiThemeProvider muiTheme={getMuiTheme(Theme)}>
+        <Provider store={store}>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/:filter" component={App} />
+                </Switch>
+            </Router>
+        </Provider>
+    </MuiThemeProvider>,
     document.getElementById("app")
 )
